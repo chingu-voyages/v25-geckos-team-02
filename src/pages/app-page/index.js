d491e5b488b292628b8504e6c147ae43797a182f
Greg Ogun <gregogun97@gmail.com>
2020-11-30 21:35:26 +0000

    first commit on branch

--- src/pages/app-page/index.js
+++ src/pages/app-page/index.js
@@ -4,46 +4,46 @@ import useFetch from "../../utils/hooks/useFetch";
 
 const App = () => {
   const { getUserDetails, getSearchItem, displayName } = useFetch();
-    // setting some state that we can update and attach to getSearchItem variables
-    const [q, setQ] = useState();
-    const [track, setTrack] = useState();
-    const [artist, setArtist] = useState();
+  // setting some state that we can update and attach to getSearchItem variables
+  const [q, setQ] = useState();
+  const [track, setTrack] = useState();
+  const [artist, setArtist] = useState();
 
-      // logging to check what we get on each re-render
+  // logging to check what we get on each re-render
   console.log(q);
   console.log(track);
   console.log(artist);
 
-    // function that handles submission of search form
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      getSearchItem({
-        q: q,
-        track: track,
-        artist: artist,
-      });
-    };
-  
-    // function to handle change in the input
-    const handleChange = (e) => {
-      setQ(e.target.value);
-    };
-  
-    // function to handle the change in select
-    const handleSelect = (e) => {
-      if (e.target.value === "track") {
-        setTrack(e.target.value);
-      }
-  
-      if (e.target.value === "artist") {
-        setArtist(e.target.value);
-      }
-    };
+  // function that handles submission of search form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getSearchItem({
+      q: q,
+      track: track,
+      artist: artist,
+    });
+  };
+
+  // function to handle change in the input
+  const handleChange = (e) => {
+    setQ(e.target.value);
+  };
+
+  // function to handle the change in select
+  const handleSelect = (e) => {
+    if (e.target.value === "track") {
+      setTrack(e.target.value);
+    }
+
+    if (e.target.value === "artist") {
+      setArtist(e.target.value);
+    }
+  };
 
   return (
     <div>
       <button onClick={() => navigate("/")}>Back to landing page</button>
-      <h1> {displayName} </h1>
+      <h1> hi {displayName} </h1>
     </div>
   );
 };
