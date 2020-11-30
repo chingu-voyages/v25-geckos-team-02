93fa4fdb5529aa709b8da93375f4922c0d4771ed
Greg Ogun <gregogun97@gmail.com>
2020-11-30 22:05:37 +0000

    chore: install chakra ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Router } from "@reach/router";
+import { ChakraProvider } from "@chakra-ui/react";
 import App from "./app-page";
 import LandingPage from "./landing-page";
 
 const Pages = () => {
   return (
-    <Router>
-      <LandingPage path="/" />
-      <App path="discover" />
-    </Router>
+    <ChakraProvider>
+      <Router>
+        <LandingPage path="/" />
+        <App path="discover" />
+      </Router>
+    </ChakraProvider>
   );
 };
 
