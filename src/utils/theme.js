fbda7a9c17a8db3ed4af0210197a52eb341c0eb7
Greg Ogun <gregogun97@gmail.com>
2020-12-02 21:19:06 +0000

    style: update custom theme object

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,6 +2,18 @@ import { theme } from "@chakra-ui/react";
 
 customTheme = {
   ...theme,
+  styles: {
+    global: (props) => ({
+      "html, body": {
+        overflowX: "hidden",
+        fontFamily: "body",
+      },
+    }),
+  },
+  fonts: {
+    heading: "Poppins, sans-serif",
+    body: "Open Sans', sans-serif",
+  },
 };
 
 export default customTheme;
