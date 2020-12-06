a016126b4c7e9ba97802ae51e788cca8bff269cc
Greg Ogun <gregogun97@gmail.com>
2020-12-06 13:43:12 +0000

    chore: add custom theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Router } from "@reach/router";
 import { ChakraProvider } from "@chakra-ui/react";
+import customTheme from "../utils/theme";
 import App from "./app-page";
 import LandingPage from "./landing-page";
 
 const Pages = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Router>
         <LandingPage path="/" />
         <App path="discover" />
