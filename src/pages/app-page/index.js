1e48954dbaa1b3e75e8c18b7ae9d3f15078181e7
Greg Ogun <gregogun97@gmail.com>
2020-12-06 13:41:26 +0000

    fix: resolve ChakraProvider ovveride

--- src/pages/app-page/index.js
+++ src/pages/app-page/index.js
@@ -1,15 +1,13 @@
-import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
-import customTheme from "../../utils/theme";
 import Header from "./header";
 import Main from "./main";
 
 const App = () => {
   return (
-    <ChakraProvider theme={customTheme}>
+    <>
       <Header />
       <Main />
-    </ChakraProvider>
+    </>
   );
 };
 
